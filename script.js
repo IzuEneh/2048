ac68d6d4c557d2607c6531bf89e010fbb1d31f20
Izu Eneh <izueneh21@gmail.com>
2022-09-28 21:43:55 -0600

    created Tile class

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import Grid from "./Grid"
+import Tile from "./Tile"
 
 const gameboard = document.getElementById("game-board")
 
 const grid = new Grid(gameboard)
+grid.randomEmptyCell(),tile = new Tile(gameboard)
